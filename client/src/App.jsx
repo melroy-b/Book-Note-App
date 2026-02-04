dc24c9876c318e1637e14ec0bfb823f7f2b056c9
Melroy Barreto <melroy.barreto@siemens.com>
2026-02-04 16:12:41 +0530

    BrowserRouter add in main.jsx, Rotes add in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import {Routes, Route} from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout.jsx";
+import Home from "../pages/Home.jsx";
 
-
-export const App = () => {
-  return <Routes>
-    <Route path="/" element={<Home/>}/>
+const App = () => {
+  <Routes element={<Layout />}>
+    <Route path="/" element={<Home />} />
   </Routes>;
 };
+
+export default App;
