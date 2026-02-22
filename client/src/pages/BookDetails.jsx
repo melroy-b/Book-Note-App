726192ee855723a3a4a319a880c707eafe5f4463
Melroy Barreto <melroybarreto61@gmail.com>
2026-02-22 22:20:58 +0530

    .env.example update, book details page edit

--- client/src/pages/BookDetails.jsx
+++ client/src/pages/BookDetails.jsx
@@ -1,4 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const BookDetails = () => {
+  const { id } = useParams();
+
   return <div>Book Details Page</div>;
 };
 
