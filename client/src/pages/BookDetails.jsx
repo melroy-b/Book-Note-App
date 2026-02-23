d0e3c2d54f5b17219e1224c0e81a48c3041063c8
Melroy Barreto <melroy.barreto@siemens.com>
2026-02-23 16:52:03 +0530

    Book Deatil page update

--- client/src/pages/BookDetails.jsx
+++ client/src/pages/BookDetails.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { use } from "react";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useFetcher, useParams } from "react-router-dom";
 
+const API_URL = import.meta.env.VITE_API_URL;
 const BookDetails = () => {
   const { id } = useParams();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const searchBookSummary = async () => {
+      if (!id) return;
+
+      try {
+        const response = await fetch(`${API_URL}/api/books/book/${id}`, { signal: controller.signal });
+        
+      } catch (error) {
+        if (error.name != "AbortError") {
+          console.error("Search for Summary failed: ", error);
+        }
+      }
+    };
+
+    searchBookSummary();
+
+    return () => controller.abort;
+  }, [id]);
 
   return <div>Book Details Page</div>;
 };
