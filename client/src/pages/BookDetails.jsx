a1742a5522d2c65020c34fd1dd9d7298d8f9a95a
Melroy Barreto <melroybarreto61@gmail.com>
2026-02-28 23:05:23 +0530

    New UI

--- client/src/pages/BookDetails.jsx
+++ client/src/pages/BookDetails.jsx
@@ -5,7 +5,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 
-const BookDetails = () => {
+const BookDetails = (props) => {
   const { id } = useParams();
 
   //Custom hooks
@@ -27,13 +27,13 @@ const BookDetails = () => {
     const description =
       typeof bookDetail?.description === "string"
         ? bookDetail.description
-        : (bookDetail.description?.value ?? "No description available.");
+        : bookDetail.description?.value ?? "No description available.";
     return description;
   }
 
   return (
     <Box sx={{ flexGrow: 1, padding: "20px" }}>
-      <Grid container spacing={1}>
+      {/* <Grid container spacing={1}>
         <Grid size={{ xs: 12, md: 5, lg: 3 }}>
           <Item>
             <Box
@@ -76,7 +76,9 @@ const BookDetails = () => {
         <Grid size={{ xs: 12, lg: 9 }}>
           <Item>
             <Box id="book-description">
-              <h2>{bookDetail?.title}</h2>
+              <h2 className="display-6 text-align-left">
+                <strong>{bookDetail?.title}</strong>
+              </h2>
               <p style={{ fontSize: "12px", textAlign: "left", maxLines: 3 }}>
                 {extractBookDescription(bookDetail)}
               </p>
@@ -106,7 +108,50 @@ const BookDetails = () => {
               <Item>Link C</Item>
             </Grid>
           </Grid>
-        </Grid> */}
+        </Grid>
+      </Grid> */}
+      <Grid container className="rounded text-body-emphasis">
+        <Grid className="p-3" size={{ lg: 9 }}>
+          <h1 className="display-6 fst-italic">{bookDetail?.title}</h1>{" "}
+          <p>
+            by {bookDetail?.authors?.map((author) => author.name).join(", ")}
+          </p>
+          <p className="my-3">{extractBookDescription(bookDetail)}</p>
+          {/* <p className="lead mb-0">
+            <a href="#" className="text-body-emphasis fw-bold">
+              Continue reading...
+            </a>
+          </p> */}
+        </Grid>
+
+        <Grid className="p-3" size={{ lg: 3 }}>
+          <Box
+            sx={{
+              padding: "5px",
+              width: "200px",
+              height: "300px",
+              borderRadius: "5px",
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <img
+              src={
+                bookDetail?.covers?.length > 0
+                  ? `https://covers.openlibrary.org/b/id/${bookDetail.covers[0]}-M.jpg`
+                  : "https://dummyimage.com/150x200/cccccc/000000&text=No+Cover"
+              }
+              alt="book-cover-main"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+              }}
+            />
+          </Box>
+        </Grid>
       </Grid>
     </Box>
   );
