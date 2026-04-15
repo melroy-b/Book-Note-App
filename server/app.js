b973dcc4bf05b0e3b2ef6052ef758139278ea4f2
Melroy Barreto <melroybarreto61@gmail.com>
2026-04-15 23:40:11 +0530

    add client app in cors

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 
 //api endpoint for routing client requests
