a38e717fc7228443e161732e9635652e2e9f8134
Melroy Barreto <melroy.barreto@siemens.com>
2026-04-17 16:56:19 +0530

    auth Route update

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/bookRoutes.js";
+import bookRouter from "./routes/bookRoutes.js";
+import authRouter from "./routes/authRoutes.js";
+
 import db from "./db/index.js";
 
 import dotenv from "dotenv";
@@ -9,9 +11,13 @@ dotenv.config();
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //api endpoint for routing client requests
-app.use("/api/books", router);
+app.use("/api/books", bookRouter);
+
+//auth endpoint for routing local and oauth flows
+app.use("/auth", authRouter);
 
 // Test database connection
 //await db.query("SELECT 1")
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,20 @@
 import express from "express";
 
-const router = express.router();
+const router = express.Router();
+
+//POST /auth/login - Initiate local auth flow
+router.post("/login", (req, res) => {
+  console.log("local login accessed");
+});
+
+//GET /auth/google - Initiate google auth flow
+router.get("/google", (req, res) => {
+  console.log("google login accessed");
+});
+
+//GET /auth/apple - Initiate apple auth flow
+router.get("/apple", (req, res) => {
+  console.log("apple login accessed");
+});
 
 export default router;
