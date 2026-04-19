bfc2f2f3e628bdbf9b3fc79cd463216559041fca
Melroy Barreto <melroybarreto61@gmail.com>
2026-04-19 22:45:48 +0530

    server session create for auth persistence

--- server/app.js
+++ server/app.js
@@ -1,17 +1,27 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
 import cors from "cors";
 import bookRouter from "./routes/bookRoutes.js";
 import authRouter from "./routes/authRoutes.js";
-
 import db from "./db/index.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //api endpoint for routing client requests
 app.use("/api/books", bookRouter);
