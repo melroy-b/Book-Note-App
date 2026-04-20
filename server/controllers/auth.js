5d487b976ed3935d3f6cc98252e242677d132ac0
Melroy Barreto <melroy.barreto@siemens.com>
2026-04-20 16:03:04 +0530

    Add Google/GitHub auth and session routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,48 +1,43 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
-import AppleStrategy from "passport-apple";
+import GithubStrategy from "passport-github2";
 import bcrypt from "bcrypt";
 import db from "../db/index.js";
+import env from "dotenv";
 
-passport.use(
-  "local",
-  new LocalStrategy(
-    { usernameField: "form_username", passwordField: "form_password" },
-    async (identifier, password, cb) => {
-      try {
-        const user = await db.query(
-          "SELECT * FROM users WHERE LOWER(email) = LOWER($1) OR LOWER(username) = LOWER($1) LIMIT 1",
-          [identifier.trim()]
-        );
+env.config();
 
-        console.log(user.rows[0]);
-        if (user.rows.length > 0) {
-          const storedHashPassword = user.rows[0].password_hash;
-          bcrypt.compare(password, storedHashPassword, (err, valid) => {
-            if (err) cb(err);
-            if (valid) return cb(null, user.rows[0]);
-            else return cb(null, false);
-          });
-        } else cb("User not found", false);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  )
-);
+export const isAuthenticated = (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json({ user: req.user });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
-export const register_user = async (req, res) => {
-  const {
-    form_username: username,
-    form_email: email,
-    form_password: password,
-  } = req.body;
-
+export const logout = (req, res) => {
   try {
+    req.logout((err) => {
+      if (err) next(err);
+      res.redirect(`${process.env.CLIENT_URL}`);
+    });
+  } catch (error) {
+    console.log("Error logging out: ", error);
+  }
+};
+
+export const q = async (req, res) => {
+  try {
+    const {
+      form_username: username,
+      form_email: email,
+      form_password: password,
+    } = req.body;
+
     const user = await db.query(
       "SELECT * FROM users WHERE LOWER(email)=LOWER($1)",
-      [email]
+      [email],
     );
 
     if (user.rows.length > 0) {
@@ -53,10 +48,11 @@ export const register_user = async (req, res) => {
         if (err) console.log("Error hashing password: ", err);
         const user = await db.query(
           "INSERT INTO users (username, password_hash, email) VALUES ($1, $2, $3) RETURNING *",
-          [username, password_hash, email]
+          [username, password_hash, email],
         );
 
         req.logIn(user.rows[0], (err) => {
+          if (err) console.log(err);
           res.redirect(`${process.env.CLIENT_URL}`);
         });
       });
@@ -65,6 +61,68 @@ export const register_user = async (req, res) => {
     console.log(error);
   }
 };
+
+passport.use(
+  "local",
+  new LocalStrategy(
+    { usernameField: "form_username", passwordField: "form_password" },
+    async (identifier, password, cb) => {
+      try {
+        const user = await db.query(
+          "SELECT * FROM users WHERE LOWER(email) = LOWER($1) OR LOWER(username) = LOWER($1) LIMIT 1",
+          [identifier.trim()],
+        );
+
+        console.log(user.rows[0]);
+        if (user.rows.length > 0) {
+          const storedHashPassword = user.rows[0].password_hash;
+          bcrypt.compare(password, storedHashPassword, (err, valid) => {
+            if (err) cb(err);
+            if (valid) return cb(null, user.rows[0]);
+            else return cb(null, false);
+          });
+        } else return cb("User not found", false);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  ),
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    async (profile, done) => {
+      console.log(profile);
+
+      try {
+        let user = await db.query(
+          "SELECT * FROM users WHERE LOWER(email) = LOWER($1) LIMIT 1",
+          [profile.email],
+        );
+
+        if (user.rows.length == 0) {
+          user = await db.query(
+            "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *",
+            [profile.displayName, profile.email, "google"],
+          );
+        }
+
+        return done(null, user.rows[0]);
+      } catch (error) {
+        console.log(error);
+        return done(error, null);
+      }
+    },
+  ),
+);
+
+passport.use("github", new GithubStrategy({}));
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
 import passport from "passport";
-import { register_user } from "../controllers/auth.js";
+import { register_user, isAuthenticated, logout } from "../controllers/auth.js";
 
 const router = express.Router();
+
+//GET /auth/me - route to check if the user is already authenticated with a session running
+router.get("/me", isAuthenticated);
+
+//GET /auth/logout - to logout from current session
+router.get("/logout", logout);
 
 //POST /auth/login - Initiate local auth flow
 router.post(
@@ -10,19 +16,27 @@ router.post(
   passport.authenticate("local", {
     successRedirect: `${process.env.CLIENT_URL}/`,
     failureRedirect: `${process.env.CLIENT_URL}/login?error=invalid_credentials`,
-  })
+  }),
 );
-
 router.post("/register", register_user);
 
 //GET /auth/google - Initiate google auth flow
-router.get("/google", (req, res) => {
-  console.log("google login accessed");
-});
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
+// /auth/google/callback - handle google callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: `${process.env.CLIENT_URL}/`,
+    failureRedirect: `${process.env.CLIENT_URL}/login?error=invalid_credentials`,
+  }),
+);
 
-//GET /auth/apple - Initiate apple auth flow
-router.get("/apple", (req, res) => {
-  console.log("apple login accessed");
+//GET /auth/github - Initiate github auth flow
+router.get("/github", (req, res) => {
+  console.log("github login accessed");
 });
 
 export default router;
